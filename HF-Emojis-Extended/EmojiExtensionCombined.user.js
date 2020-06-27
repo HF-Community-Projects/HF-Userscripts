6610de28a459fcc82f5d242d1c1c10e56a6fefe0
Cain <gothangelxxx@gmail.com>
2020-06-27 07:22:03 +0100

    Fixed everything with 2 lines of code.
    Fixed images from being stretched out and also only use 1 URL now.

--- HF-Emojis-Extended/EmojiExtensionCombined.user.js
+++ HF-Emojis-Extended/EmojiExtensionCombined.user.js
@@ -76,19 +76,20 @@
         })
     }
 
+    // Utility Functions
     function PopulateCustomEmojiArray() {
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/ph9Jm7c/nymnCorn.gif", "https://cdn.betterttv.net/emote/56cb56f5500cb4cf51e25b90/1x", "nymnCorn"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/yWy5RKp/peepo-Dance.gif", "https://cdn.betterttv.net/emote/5a6edb51f730010d194bdd46/1x", "peepoDance"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/N7rB0vT/peepoSad.png", "https://cdn.betterttv.net/emote/5a16ddca8c22a247ead62ceb/1x", "peepoSad"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/jfzn4Vk/pepeD.gif", "https://cdn.betterttv.net/emote/5b1740221c5a6065a7bad4b5/1x", "pepeD"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/rk3hHHs/Pepega.png", "https://cdn.betterttv.net/emote/5aca62163e290877a25481ad/1x", "Pepega"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/MgNHmhP/Pepe-Hands.png", "https://cdn.betterttv.net/emote/59f27b3f4ebd8047f54dee29/1x", "PepeHands"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/5M0HWzt/pepeJam.gif", "https://cdn.betterttv.net/emote/5b77ac3af7bddc567b1d5fb2/1x", "pepeJam"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/X4CZnSp/Pepe-Laugh.gif", "https://cdn.betterttv.net/emote/5c548025009a2e73916b3a37/1x", "PepeLaugh"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/hVRrB5s/omegalul.png", "https://cdn.betterttv.net/emote/583089f4737a8e61abb0186b/1x", "OMEGALUL"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/zX0JKKJ/clap.gif", "https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/1x", "Clap"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/JCjQS67/KEKW.png", "https://cdn.betterttv.net/emote/5e9c6c187e090362f8b0b9e8/1x", "KEKW"));
-        customEmojiList.push(new CustomEmojiObject("https://i.ibb.co/x1G4kW3/weird-Champ.png", "https://cdn.betterttv.net/emote/5d20a55de1cfde376e532972/1x", "weirdChamp"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/56cb56f5500cb4cf51e25b90/1x", "nymnCorn"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5a6edb51f730010d194bdd46/1x", "peepoDance"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5a16ddca8c22a247ead62ceb/1x", "peepoSad"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5b1740221c5a6065a7bad4b5/1x", "pepeD"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5aca62163e290877a25481ad/1x", "Pepega"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/59f27b3f4ebd8047f54dee29/1x", "PepeHands"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5b77ac3af7bddc567b1d5fb2/1x", "pepeJam"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5c548025009a2e73916b3a37/1x", "PepeLaugh"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/583089f4737a8e61abb0186b/1x", "OMEGALUL"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/1x", "Clap"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5e9c6c187e090362f8b0b9e8/1x", "KEKW"));
+        customEmojiList.push(new CustomEmojiObject("https://cdn.betterttv.net/emote/5d20a55de1cfde376e532972/1x", "weirdChamp"));
         // Total Extra = 12
     }
 
@@ -96,11 +97,13 @@
         customEmojiList.forEach(emoji => {
             let emojiElement = document.createElement('img');
             emojiElement.style = emoji.style;
-            emojiElement.src = emoji.displaySrc;
+            emojiElement.width = 25;
+            emojiElement.height = 25;
+            emojiElement.src = emoji.imgSrc;
             emojiElement.title = emoji.title;
 
             emojiElement.onclick = function () {
-                MyBBEditor.insertText(`[img]${emoji.realSrc}[/img]`)
+                MyBBEditor.insertText(`[img]${emoji.imgSrc}[/img]`)
             }
 
             if (document.title.includes("Post Reply")) {
